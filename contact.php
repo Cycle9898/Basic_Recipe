b0932ac5a9024ae00a5a60b248e14cbeabb63bc6
Cycle9898 <cycle9898@gmail.com>
2024-06-03 17:03:23 +0200

    adding file upload on contact form

--- contact.php
+++ contact.php
@@ -14,19 +14,25 @@
 
         <h1 class="text-center mt-5 mb-5">Contactez nous</h1>
 
-        <form action="submit_contact.php" method="POST">
+        <form action="submit_contact.php" method="POST" enctype="multipart/form-data">
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
 
                 <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help">
 
-                <div id="email-help" class="form-text">Nous ne revendrons pas votre email.</div>
+                <p id="email-help" class="form-text">Nous ne revendrons pas votre email.</p>
             </div>
 
             <div class="mb-3">
                 <label for="message" class="form-label">Votre message</label>
 
                 <textarea class="form-control" placeholder="Exprimez vous" id="message" name="message"></textarea>
+            </div>
+
+            <div class="mb-3">
+                <label for="screenshot" class="form-label">Votre capture d'écran</label>
+                
+                <input type="file" class="form-control" id="screenshot" name="screenshot" />
             </div>
 
             <button type="submit" class="btn btn-primary m-auto d-block">Envoyer</button>
--- submit_contact.php
+++ submit_contact.php
@@ -1,43 +1,69 @@
+<?php
+$postData = $_POST;
+
+// Test if fields are corrects
+if (
+    !isset($postData['email'])
+    || !filter_var($postData['email'], FILTER_VALIDATE_EMAIL)
+    || empty($postData['message'])
+    || trim($postData['message']) === ''
+) {
+    echo('Il faut un email et un message valides pour soumettre le formulaire.');
+    return;
+}
+
+$isFileLoaded = false;
+// Test if uploaded file is correct
+if (isset($_FILES['screenshot']) && $_FILES['screenshot']['error'] === 0) {
+    // Size
+    if ($_FILES['screenshot']['size'] > 1000000) {
+        echo "L'envoi n'a pas pu être effectué, erreur ou image trop volumineuse";
+        return;
+    }
+
+    // Extension
+    $fileInfo = pathinfo($_FILES['screenshot']['name']);
+    $extension = $fileInfo['extension'];
+    $allowedExtensions = ['jpg', 'jpeg', 'gif', 'png'];
+    if (!in_array($extension, $allowedExtensions)) {
+        echo "L'envoi n'a pas pu être effectué, l'extension {$extension} n'est pas autorisée";
+        return;
+    }
+
+    // Target folder
+    $path = __DIR__ . '/assets/uploads/';
+    if (!is_dir($path)) {
+        echo "L'envoi n'a pas pu être effectué, le dossier uploads est manquant";
+        return;
+    }
+
+    // Save file
+    move_uploaded_file($_FILES['screenshot']['tmp_name'], $path . basename($_FILES['screenshot']['name']));
+    $isFileLoaded = true;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Site de recettes - Contact</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <?php
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-
-    if (
-        !isset($email)
-        || !filter_var($email, FILTER_VALIDATE_EMAIL)
-        || !isset($message)
-        || trim($message) === ''
-        ) : ?>
-    <h1>Un problème est survenu !</h1>
-
-    <div class="card">
-        <p>Il faut un email et un message valides pour soumettre le formulaire.</p>
-    </div>
-
-    <a href="contact.php">Retour au formulaire</a>
-    
-    <?php else : ?>
+    <div class="container">
         <h1>Message bien reçu !</h1>
-
-    <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">Rappel de vos informations</h5>
-            <p class="card-text"><b>Email</b> : <?php echo $email; ?></p>
-            <p class="card-text"><b>Message</b> : <?php echo strip_tags($message); ?></p>
+        
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Rappel de vos informations</h5>
+                <p class="card-text"><b>Email</b> : <?php echo $postData['email']; ?></p>
+                <p class="card-text"><b>Message</b> : <?php echo strip_tags($postData['message']); ?></p>
+            </div>
         </div>
+        
+        <a href="index.php">Retour à l'accueil</a>
     </div>
-
-    <a href="index.php">Retour à l'accueil</a>
-    
-    <?php endif ?>
-
 </body>
 </html>
